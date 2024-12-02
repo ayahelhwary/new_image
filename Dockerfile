8ad95288d324462888b40ebfb9e0f46ee5b69530
ayahelhwary <elhwarya810@gmail.com>
2024-12-02 16:21:54 +0200

    Added files

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk
+
+WORKDIR /app
+
+COPY test.java .
+
+RUN javac test.java
+
+CMD java test
---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("WELCOOOOME");
+        System.out.println("HELLLLLLLLLLLO");
+        System.out.println("my nameeee AYAAAA");
+    }
+}
+
